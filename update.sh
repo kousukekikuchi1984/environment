cdbfb2edee7e4b66ac28a6eae2240d8177aef0e6
Kosuke Kikuchi <kosuke-kikuchi@platform-one.co.jp>
2016-02-04 16:58:16 +0900

    automation

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
-cp zshrc ~/.zshrc
+## vim
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
 cp vimrc ~/.vimrc
+
+## zsh
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+cp zshrc ~/.zshrc
