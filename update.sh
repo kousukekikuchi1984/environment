412b35b9e427d21a3df2dfa69e7b04cf7174be9a
Kousuke Kikuchi <kousukekikuchi@toki.waseda.jp>
2016-02-07 10:40:41 +0900

    configuration automation

--- update.sh
+++ update.sh
@@ -5,4 +5,8 @@ cp vimrc ~/.vimrc
 
 ## zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 cp zshrc ~/.zshrc
+source .zshrc
